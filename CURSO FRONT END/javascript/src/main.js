80d6d1dc47a9843c78fe64295c29672d85a7f02c
rodrigoft91 <107896164+rodrigoft91@users.noreply.github.com>
2022-06-28 23:08:05 -0300

    Add files via upload

--- CURSO FRONT END/javascript/src/main.js
+++ CURSO FRONT END/javascript/src/main.js
@@ -1,1 +1,19 @@
-alert ('Seja bem vindo ao curso');
+//alert ('Seja bem vindo ao curso');
+
+// leitura de variaveis
+
+var nome = prompt('Qual seu nome: ')
+
+var idade = prompt('qual sua idade: ')
+
+document.write('<h1>Seu nome é ' + nome + ', e sua idade é ' + idade + ' anos.</h1>')
+
+ 
+// alerta na página
+
+alert('Seu nome é ' + nome + '!')   
+alert('Sua idade é ' + idade + '!')
+
+// console
+
+console.log('nome: ' + nome + ', idade: ' + idade + '!')
